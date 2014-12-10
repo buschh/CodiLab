db3c072a776c763e46737d40a35ae5106bb9c4c5
JochenGraf <jochen.graf@uni-koeln.de>
2014-12-10 14:58:21 +0100

    CodiKOS editor started

---
+++ build/deps.bat
@@ -0,0 +1,5 @@
+..\..\HKIKoeln\SemToNotes\client\lib\closure-library\closure\bin\build\depswriter.py ^
+  --root_with_prefix="..\..\HKIKoeln\SemToNotes\client\src ..\..\..\..\..\client\src" ^
+  > .\client\deps.js
+
+@echo "Dependency file updated"
