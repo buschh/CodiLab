0c47775de8f6a661d96a1966f5deb240a68ed04b
Jochen <jochen.graf@uni-koeln.de>
2014-12-16 09:19:04 +0100

    CodiKOS editor continued

---
+++ build/deps.sh
@@ -0,0 +1,3 @@
+../../HKIKoeln/SemToNotes/client/lib/closure-library/closure/bin/build/depswriter.py \
+  --root_with_prefix="../../HKIKoeln/SemToNotes/client/src ../../../../../client/src" \
+  > ./client/deps.js
